1b55e4e9180ecf1f0e9362e4c64ad436c4bef406
Nyzuro <marco.strs27@gmail.com>
2024-12-28 23:51:33 +0100

    add function

--- eau00.js
+++ eau00.js
@@ -1,12 +1,14 @@
-let a = 0
-
-for (let b; a >= 0 && a <= 7; a++) {
-    b = a + 1
-    for (let c; b >= 1 && b <= 8; b++) {
-        c = b + 1
-        for (; c >= 2 && c <= 9; c++) {
-            const combinations = `${a}${b}${c}`
-            console.log(combinations)
+function threeNumbersCombs() {
+    let a = 0
+    for (let b; a >= 0 && a <= 7; a++) {
+        b = a + 1
+        for (let c; b >= 1 && b <= 8; b++) {
+            c = b + 1
+            for (; c >= 2 && c <= 9; c++) {
+                const combinations = `${a}${b}${c}`
+                console.log(combinations)
+            }
         }
     }
 }
+threeNumbersCombs()
