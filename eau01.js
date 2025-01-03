2046d0497dd4518b5e1afb15fb0d8bbb8cc25c6e
Nyzuro <marco.strs27@gmail.com>
2025-01-03 16:50:25 +0100

    modifs

--- eau01.js
+++ eau01.js
@@ -10,11 +10,11 @@ function twoNumbersCombs() {
             for (; k >= 0 && k <= 9; k++) {
                 l = 0
                 for (; l >= 0 && l <= 9; l++) {
-                    if (i <= k) { // il faut check si le est bien <= au troisieme sinon c'est pas bon
+                    if (i <= k) { // il faut check si le 1er est bien <= au 3eme sinon c'est pas bon
                         if (i < k) {
                             const comb = `${i}${j} ${k}${l}`
                             console.log(comb)
-                        } else if (j < l) { // dans le cas ou il est egal on check si le deuxieme est inferieur au troisieme sinon c'est pas bon
+                        } else if (j < l) { // dans le cas ou il est egal on check si le 2eme est inferieur au 3eme sinon c'est pas bon
                             const comb = `${i}${j} ${k}${l}`
                             console.log(comb)
                         }
