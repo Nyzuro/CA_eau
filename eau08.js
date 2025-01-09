49dc8a9e6a2e569891cabdb03e1cb9844bfe47ec
Nyzuro <marco.strs27@gmail.com>
2025-01-09 17:54:41 +0100

    eau08

---
+++ eau08.js
@@ -0,0 +1,22 @@
+const arguments = process.argv.slice(2)
+
+function checkNumber() {
+    if (Number(string)) {
+        return true
+    }
+    for (i in string) {
+        charAscii = string.charCodeAt(i)
+        if (charAscii < 48 || charAscii > 57) {
+            return false
+        }
+    }
+    return true
+}
+
+if (arguments.length != 1) {
+    console.error("error")
+    process.exit()
+}
+
+const string = arguments[0]
+console.log(checkNumber(string))
