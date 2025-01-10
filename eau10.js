521f864ebba78b2f8d0eb7666b802ac0a7638c19
Nyzuro <marco.strs27@gmail.com>
2025-01-10 19:40:43 +0100

    eau10

--- eau10.js
+++ eau10.js
@@ -3,6 +3,16 @@ const characters = process.argv.slice(2)
 function indexWanted() {
     const wanted = characters.pop()
     const array = characters
+    const find = array.indexOf(wanted)
+    if (find === -1) {
+        return find
+    }
+    return find
 }
 
-indexWanted(characters)
+if (characters.length < 2) {
+    console.error("error")
+    process.exit()
+}
+
+console.log(indexWanted(characters))
