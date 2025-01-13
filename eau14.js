7b4eae696a571e3405de889e597810cd7ad8770f
Nyzuro <152918886+Nyzuro@users.noreply.github.com>
2025-01-13 20:12:37 +0100

    Add files via upload

---
+++ eau14.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2)
+
+function asciiSort(array) {
+    for (i = array.length - 1; i >= 1; i++) {
+        for (j = 0; j <= i - 1; j++) {
+
+        }
+    }
+}
+
+asciiSort(args)
