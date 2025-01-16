371fae333e80dcd4b75191405627fc3cab2d3e2f
Nyzuro <marco.strs27@gmail.com>
2025-01-16 12:52:43 +0100

    eau14

--- eau14.js
+++ eau14.js
@@ -1,11 +1,29 @@
 const args = process.argv.slice(2)
 
 function asciiSort(array) {
-    for (i = array.length - 1; i >= 1; i++) {
+    for (i = array.length - 1; i >= 1; i--) {
         for (j = 0; j <= i - 1; j++) {
-
+            for (k in array[j]) {
+                const charAscii = array[j].charCodeAt(k)
+                const nextCharAscii = array[j + 1].charCodeAt(k)
+                if (charAscii < nextCharAscii) {
+                    break
+                }
+                if (charAscii > nextCharAscii) {
+                    const temp = array[j]
+                    array[j] = array[j + 1]
+                    array[j + 1] = temp
+                    break
+                }
+            }
         }
     }
+    return array
 }
 
-asciiSort(args)
+if (args.length < 1) {
+    console.error("error")
+    process.exit()
+}
+
+console.log(asciiSort(args))
