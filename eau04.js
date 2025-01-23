68ae38a46296a713768db3ba735a2aa609ff943e
Nyzuro <marco.strs27@gmail.com>
2025-01-23 15:23:13 +0100

    Prochain nombre premier

--- eau04.js
+++ eau04.js
@@ -1,25 +1,49 @@
-const arguments = process.argv.slice(2)
-//Fonctions
-function findFirstNumber(numberTest) {
-    numberTest++
-    for (let count; count != 2; numberTest++) {
-        count = 0
-        for (i = 1; i <= numberTest; i++) {
+//Utils
+function nextPrimeNumber(number) {
+    if (number < 2) {
+        return 2
+    }
+    let isPrime = false
+    let numberTest
+    for (numberTest = number + 1; isPrime === false; numberTest++) {
+        for (let i = 2; i < numberTest; i++) {
             if (numberTest % i === 0) {
-                count++
+                isPrime = false
+                break
+            } else {
+                isPrime = true
             }
         }
     }
     return numberTest - 1
 }
 //Gestion d'erreurs
-if (arguments.length != 1 || isNaN(arguments) || arguments < 0) {
-    console.error(-1)
-    process.exit()
+function isValidArguments(arguments) {
+    if (arguments.length != 1) {
+        console.error("Le nombre d'arguments n'est pas bon")
+        process.exit()
+    } else {
+        return arguments
+    }
+}
+
+function isValidNumber(number) {
+    if (!isNaN(number)) {
+        return Number(number)
+    } else {
+        console.error("L'argument n'est pas un nombre")
+        process.exit()
+    }
 }
 //Parsing
-let numberTest = arguments[0]
+function getArguments() {
+    const arguments = process.argv.slice(2)
+    return arguments
+}
 //Resolution
-const firstNumber = findFirstNumber(numberTest)
+function getNextPrimeNumber() {
+    const number = isValidNumber(isValidArguments(getArguments()))
+    return nextPrimeNumber(number)
+}
 //Affichage
-console.log(firstNumber)
+console.log(getNextPrimeNumber())
