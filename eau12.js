7ad953caecaff1478b9a0107c47fc739c53af966
Nyzuro <marco.strs27@gmail.com>
2025-01-24 18:47:28 +0100

    fix error handle

--- eau12.js
+++ eau12.js
@@ -1,9 +1,8 @@
 const args = process.argv.slice(2)
 
 function bubble_sort(array) {
-    for (i = array.length; i >= 1; i--) {
-        let j = 0
-        for (; j <= i - 1; j++) {
+    for (let i = array.length; i >= 1; i--) {
+        for (let j = 0; j <= i - 1; j++) {
             if (array[j] > array[j + 1]) {
                 let temp = array[j]
                 array[j] = array[j + 1]
@@ -14,18 +13,17 @@ function bubble_sort(array) {
     return array
 }
 
-const argsNumber = []
-for (i of args) {
-    const currentNumber = parseInt(i)
-    if (isNaN(currentNumber)) {
-        console.error("error")
+const numbers = []
+for (let arg of args) {
+    if (isNaN(arg)) {
+        console.error("Ce ne sont pas des nombres")
         process.exit()
     }
-    argsNumber.push(currentNumber)
+    numbers.push(Number(arg))
 }
 if (args.length < 2) {
-    console.error("error")
+    console.error("Le nombre d'arguments est trop faible")
     process.exit()
 }
 
-console.log(bubble_sort(argsNumber))
+console.log(bubble_sort(numbers))
