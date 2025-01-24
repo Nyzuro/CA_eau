5c8f884fa0b69e03b26119c7e19671e8cb27ad88
Nyzuro <marco.strs27@gmail.com>
2025-01-24 18:15:04 +0100

    refactor all the exercice

--- eau10.js
+++ eau10.js
@@ -1,18 +1,46 @@
-const characters = process.argv.slice(2)
-
-function indexWanted() {
-    const wanted = characters.pop()
-    const array = characters
-    const find = array.indexOf(wanted)
-    if (find === -1) {
-        return find
+function indexWanted(strings, wanted) {
+    let isSameChar = false
+    let i = 0
+    while (!isSameChar) {
+        for (let j = 0; strings[i][j]; j++) {
+            if (strings[i][j] === wanted[0]) {
+                for (const char of wanted) {
+                    if (strings[i][j] === char) {
+                        isSameChar = true
+                        j++
+                    } else {
+                        isSameChar = false
+                        break
+                    }
+                }
+            }
+        }
+        i++
     }
-    return find
+    if (!isSameChar && !strings[i]) {
+        return -1
+    }
+    return i - 1
 }
 
-if (characters.length < 2) {
-    console.error("error")
-    process.exit()
+function isValidArguments(arguments) {
+    if (arguments.length < 2) {
+        console.error("Le nombre d'arguments est trop faible")
+        process.exit()
+    }
+    return arguments
 }
 
-console.log(indexWanted(characters))
+function getArguments() {
+    const arguments = process.argv.slice(2)
+    return arguments
+}
+
+function findIndexWanted() {
+    const arrayOfStrings = isValidArguments(getArguments())
+    const wanted = arrayOfStrings.pop()
+    const strings = arrayOfStrings
+    return indexWanted(strings, wanted)
+}
+
+console.log(findIndexWanted())
