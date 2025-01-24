dfdc0d9eef2688cc8ad0d65e6fcb9ba236881c46
Nyzuro <marco.strs27@gmail.com>
2025-01-24 10:03:45 +0100

    Majuscule sur deux

--- eau06.js
+++ eau06.js
@@ -1,34 +1,63 @@
-const arguments = process.argv.slice(2)
-
-function uppercase() {
-    let result = ""
-    let lastLetter = 0
-    for (i in string) {
-        let charAscii = string.charCodeAt(i)
-        if ((lastLetter === 0) &&
-            (charAscii >= 97 && charAscii <= 122)) {
-            charAscii -= 32
-        }
-        if ((lastLetter >= 97 && lastLetter <= 122) &&
-            (charAscii >= 97 && charAscii <= 122)) {
-            charAscii -= 32
-        }
-        else if ((lastLetter >= 65 && lastLetter <= 90)
-            && (charAscii >= 65 && charAscii <= 90)) {
-            charAscii += 32
-        }
-        if (charAscii >= 65 && charAscii <= 90 || charAscii >= 97 && charAscii <= 122) {
-            lastLetter = charAscii
-        }
-        result += String.fromCharCode(charAscii)
+function isLower(character) {
+    if (character >= "a" && character <= "z") {
+        return true
     }
-    return result
+    return false
 }
 
-if (arguments.length != 1 || Number(arguments[0])) {
-    console.error("error")
-    process.exit()
+function isUpper(character) {
+    if (character >= "A" && character <= "Z") {
+        return true
+    }
+    return false
 }
 
-const string = arguments[0]
-console.log(uppercase(string))
+function upperOrLower(string) {
+    let finalString = ""
+    let isLastCharUpper = false
+    for (let char of string) {
+        if (isLastCharUpper) {
+            if (isUpper(char)) {
+                char = char.toLowerCase()
+                isLastCharUpper = false
+            } else if (isLower(char)) {
+                isLastCharUpper = false
+            }
+            finalString += char
+        }
+        else {
+            if (isLower(char)) {
+                char = char.toUpperCase()
+                isLastCharUpper = true
+            } else if (isUpper(char)) {
+                isLastCharUpper = true
+            }
+            finalString += char
+        }
+    }
+    return finalString
+}
+
+function isValidArguments(arguments) {
+    if (arguments.length != 1) {
+        console.error("Le nombre d'arguments n'est pas bon")
+        process.exit()
+    } if (!isNaN(arguments[0])) {
+        console.error("L'argument ne doit pas etre un nombre")
+        process.exit()
+    }
+    return arguments
+}
+
+function getArguments() {
+    const arguments = process.argv.slice(2)
+    return arguments
+}
+
+function displayUpperOrLower() {
+    const strings = isValidArguments(getArguments())
+    console.log(strings)
+    return upperOrLower(strings[0])
+}
+
+console.log(displayUpperOrLower())
