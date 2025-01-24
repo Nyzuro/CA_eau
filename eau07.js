9ca8aad8e079379845816f30bdc2181e60e64921
Nyzuro <marco.strs27@gmail.com>
2025-01-24 10:18:46 +0100

    Majuscule

--- eau07.js
+++ eau07.js
@@ -1,38 +1,43 @@
 const arguments = process.argv.slice(2)
 
-function checkLetter() {
-    if ((charBefore >= 65 && charBefore <= 90) || (charBefore >= 97 && charBefore <= 122)) {
+function toLower(charAsciiBefore, charAscii) {
+    if ((charAsciiBefore >= 65 && charAsciiBefore <= 90) || (charAsciiBefore >= 97 && charAsciiBefore <= 122)) {
         if (charAscii >= 65 && charAscii <= 90) {
             charAscii += 32
         }
     }
+    return charAscii
 }
 
-function checkWhiteSpaces() {
-    if (charBefore >= 9 && charBefore <= 13 || charBefore === 32) {
+function toUpper(charAsciiBefore, charAscii) {
+    if (charAsciiBefore >= 9 && charAsciiBefore <= 13 || charAsciiBefore === 32) {
         if (charAscii >= 97 && charAscii <= 122) {
             charAscii -= 32
         }
     }
+    return charAscii
 }
 
-function uppercase() {
+function uppercase(string) {
     let result = ""
-    for (i in string) {
-        charAscii = string.charCodeAt(i)
-        charBefore = string.charCodeAt(i - 1)
-        if (!charBefore && (charAscii >= 97 && charAscii <= 122)) {
+    for (let i = 0; string[i]; i++) {
+        let charAscii = string.charCodeAt(i)
+        let charAsciiBefore = string.charCodeAt(i - 1)
+        if (!charAsciiBefore && (charAscii >= 97 && charAscii <= 122)) {
             charAscii -= 32
         }
-        checkWhiteSpaces(charBefore, charAscii)
-        checkLetter(charBefore, charAscii)
+        charAscii = toUpper(charAsciiBefore, charAscii)
+        charAscii = toLower(charAsciiBefore, charAscii)
         result += String.fromCharCode(charAscii)
     }
     return result
 }
 
-if (arguments.length != 1 || Number(arguments[0])) {
-    console.error("error")
+if (arguments.length != 1) {
+    console.error("Le nombre d'arguments n'est pas bon")
+    process.exit()
+} if (!isNaN(arguments[0])) {
+    console.error("L'argument ne doit pas etre un nombre")
     process.exit()
 }
 
